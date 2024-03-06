3914e59597e9a6684ae96c1abad305939e262b5f
Anton Sidorov <antosid@gmail.com>
2024-03-06 17:37:24 +0000

    Update dependencies and add command-line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,37 @@
+use clap::Parser;
+use env_logger;
+use log::{debug, info};
 use openssl::ssl::{SslConnector, SslMethod};
-use openssl::ssl::SslStream;
 use std::net::TcpStream;
 
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+#[clap(
+    version = "v0.0.1",
+    author = "Anton Sidorov tonysidrock@gmail.com",
+    about = "Security scanner"
+)]
+struct Args {
+    #[clap(short, long, required = true)]
+    address: String,
+}
+
 fn main() {
-    // Define the URL of the website you want to check
-    let host = "www.google.com";
+    env_logger::init();
+
+    let args = Args::parse();
+
+    let host: &str = &args.address;
     let port = 443;
+    debug!("Connecting to {}:{}", host, port);
 
     // Create an SSL connector with default options
     let ssl_connector = SslConnector::builder(SslMethod::tls()).unwrap().build();
+    debug!("SSL connector created successfully");
 
     // Connect to the server
     let stream = TcpStream::connect((host, port)).unwrap();
-    let ssl_stream = ssl_connector.connect(host, stream).unwrap();
+    let ssl_stream = ssl_connector.connect(&host, stream).unwrap();
 
     // Get the SSL connection's current cipher and protocol
     let ssl = ssl_stream.ssl();
