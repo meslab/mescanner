056f7a879d4e45320c56dc8a6415eb5a1c8d7abd
Anton Sidorov <antosid@gmail.com>
2024-03-07 08:31:45 +0000

    security-level: Update version to 0.0.2 and fix SSL/TLS connection issues

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 use clap::Parser;
-use env_logger;
-use log::{debug, info};
+use log::debug;
 use openssl::ssl::{SslConnector, SslMethod, SslVerifyMode};
 use std::net::TcpStream;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 #[clap(
-    version = "v0.0.1",
+    version = "v0.0.2",
     author = "Anton Sidorov tonysidrock@gmail.com",
     about = "Security scanner"
 )]
@@ -27,7 +26,7 @@ impl TlsVersions {
     fn new() -> Self {
         TlsVersions {
             versions: vec![
-                openssl::ssl::SslVersion::SSL3,
+                // openssl::ssl::SslVersion::SSL3,
                 openssl::ssl::SslVersion::TLS1,
                 openssl::ssl::SslVersion::TLS1_1,
                 openssl::ssl::SslVersion::TLS1_2,
@@ -38,34 +37,43 @@ impl TlsVersions {
 
     fn try_connect(&self, host: &str, port: u16) {
         for &tls_version in &self.versions {
-            let mut ssl_connector_builder = SslConnector::builder(SslMethod::tls()).unwrap();
-            // ssl_connector_builder.set_verify(SslVerifyMode::NONE);
+            let mut ssl_connector_builder = SslConnector::builder(SslMethod::tls_client()).unwrap();
+            ssl_connector_builder.set_verify(SslVerifyMode::NONE);
+            ssl_connector_builder.set_security_level(0);
+            match tls_version {
+                openssl::ssl::SslVersion::TLS1_3 => {}
+                _ => ssl_connector_builder.set_cipher_list("ALL").unwrap(),
+            }
+            ssl_connector_builder
+                .set_min_proto_version(Some(tls_version))
+                .unwrap();
+            ssl_connector_builder
+                .set_max_proto_version(Some(tls_version))
+                .unwrap();
             let ssl_connector = ssl_connector_builder.build();
 
             match TcpStream::connect((host, port)) {
-                Ok(stream) => {
-                    match ssl_connector.connect(host, stream) {
-                        Ok(ssl_stream) => {
-                            debug!("SSL stream: {:?}", ssl_stream);
-                            let ssl = ssl_stream.ssl();
-                            println!(
-                                "Connected using TLS version: {}",
-                                self.tls_version_to_string(tls_version)
-                            );
-                            println!("Supported Ciphers:");
-                            for cipher in ssl.current_cipher().unwrap().name().split(':') {
-                                println!("{}", cipher);
-                            }
-                        }
-                        Err(err) => {
-                            println!(
-                                "Connection attempt using TLS {} failed: {}",
-                                self.tls_version_to_string(tls_version),
-                                err
-                            );
+                Ok(stream) => match ssl_connector.connect(host, stream) {
+                    Ok(ssl_stream) => {
+                        debug!("SSL stream: {:?}", ssl_stream);
+                        let ssl = ssl_stream.ssl();
+                        println!(
+                            "Connected using TLS version: {}",
+                            self.tls_version_to_string(tls_version)
+                        );
+                        println!("Supported Ciphers:");
+                        for cipher in ssl.current_cipher().unwrap().name().split(':') {
+                            println!("{}", cipher);
                         }
                     }
-                }
+                    Err(err) => {
+                        println!(
+                            "Connection attempt using TLS {} failed: {}",
+                            self.tls_version_to_string(tls_version),
+                            err
+                        );
+                    }
+                },
                 Err(err) => {
                     println!(
                         "Connection attempt using TLS {} failed: {}",
