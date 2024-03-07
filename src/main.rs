941ba8a059358f3ae783e8cb4b28991642e5d78c
Anton Sidorov <antosid@gmail.com>
2024-03-07 07:40:53 +0000

    security-level: Fix TLS connection issue by removing SSLv3 support

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use clap::Parser;
 use env_logger;
 use log::{debug, info};
-use openssl::ssl::{SslConnector, SslMethod};
+use openssl::ssl::{SslConnector, SslMethod, SslVerifyMode};
 use std::net::TcpStream;
 
 #[derive(Parser)]
@@ -27,7 +27,6 @@ impl TlsVersions {
     fn new() -> Self {
         TlsVersions {
             versions: vec![
-                openssl::ssl::SslVersion::SSL3,
                 openssl::ssl::SslVersion::TLS1,
                 openssl::ssl::SslVersion::TLS1_1,
                 openssl::ssl::SslVersion::TLS1_2,
@@ -39,36 +38,33 @@ impl TlsVersions {
     fn try_connect(&self, host: &str, port: u16) {
         for &tls_version in &self.versions {
             let mut ssl_connector_builder = SslConnector::builder(SslMethod::tls()).unwrap();
-            ssl_connector_builder
-                .set_min_proto_version(Some(tls_version))
-                .unwrap();
-            ssl_connector_builder
-                .set_max_proto_version(Some(tls_version))
-                .unwrap();
+            // ssl_connector_builder.set_verify(SslVerifyMode::NONE);
             let ssl_connector = ssl_connector_builder.build();
 
             match TcpStream::connect((host, port)) {
-                Ok(stream) => match ssl_connector.connect(host, stream) {
-                    Ok(ssl_stream) => {
-                        debug!("SSL stream: {:?}", ssl_stream);
-                        let ssl = ssl_stream.ssl();
-                        println!(
-                            "Connected using TLS version: {}",
-                            self.tls_version_to_string(tls_version)
-                        );
-                        println!("Supported Ciphers:");
-                        for cipher in ssl.current_cipher().unwrap().name().split(':') {
-                            println!("{}", cipher);
+                Ok(stream) => {
+                    match ssl_connector.connect(host, stream) {
+                        Ok(ssl_stream) => {
+                            debug!("SSL stream: {:?}", ssl_stream);
+                            let ssl = ssl_stream.ssl();
+                            println!(
+                                "Connected using TLS version: {}",
+                                self.tls_version_to_string(tls_version)
+                            );
+                            println!("Supported Ciphers:");
+                            for cipher in ssl.current_cipher().unwrap().name().split(':') {
+                                println!("{}", cipher);
+                            }
+                        }
+                        Err(err) => {
+                            println!(
+                                "Connection attempt using TLS {} failed: {}",
+                                self.tls_version_to_string(tls_version),
+                                err
+                            );
                         }
                     }
-                    Err(err) => {
-                        println!(
-                            "Connection attempt using TLS {} failed: {}",
-                            self.tls_version_to_string(tls_version),
-                            err
-                        );
-                    }
-                },
+                }
                 Err(err) => {
                     println!(
                         "Connection attempt using TLS {} failed: {}",
@@ -82,7 +78,6 @@ impl TlsVersions {
 
     fn tls_version_to_string(&self, tls_version: openssl::ssl::SslVersion) -> &'static str {
         match tls_version {
-            openssl::ssl::SslVersion::SSL3 => "SSLv3",
             openssl::ssl::SslVersion::TLS1 => "TLSv1",
             openssl::ssl::SslVersion::TLS1_1 => "TLSv1.1",
             openssl::ssl::SslVersion::TLS1_2 => "TLSv1.2",
